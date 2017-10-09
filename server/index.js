1deceb2f3b14aa9eddf712b4868550fbd41e96a2
Felix Faerber <ffaerber@gmail.com>
2017-10-09 20:18:23 +0000

    more logs

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 var io = require('socket.io')();
-
+console.log('starting...')
 io.on('connection', (socket) => {
+  console.log('connected')
   socket.on('message', (message) => {
     console.log(message);
   });
